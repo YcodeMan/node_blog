11beeadcc2f17a416ae2ffad7f45708cec109526
YcodeMan <1130578809@qq.com>
2019-05-15 20:28:21 +0800

    add / routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main/index', { title: 'Express' });
+  res.render('main/index.ejs', { title: 'Express' });
 });
+
+
+
 
 module.exports = router;
