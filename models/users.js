5390a3ad0b70b687498f20eaa756f11eadca66a6
YcodeMan <1130578809@qq.com>
2019-05-15 21:17:18 +0800

    delete modls

---
+++ models/users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+   username: String,
+   password: String,
+   isAdmin: {
+      type: Boolean,
+      default: false 
+   }
+})
+module.exports = mongoose.model( 'user', userSchema, 'user')
