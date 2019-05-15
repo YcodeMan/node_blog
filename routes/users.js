6780d79bf07f7ab6784b7cd956cf106d71c1f1a9
YcodeMan <1130578809@qq.com>
2019-05-15 21:16:13 +0800

    modify users.js

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost/shop", (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(" connect ok")
+    }
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a  aaa');
+    res.render('main/index.ejs', { title: 'Express' });
 });
 
 // 配置注册页面模板
 router.get("/sign_up", (req, res, next) => {
-   res.render('components/signup.ejs', {title: 'signup'})
+  res.render('components/signup.ejs', {title: 'signup'})
 })
 // 配置登录模板页面
 router.get("/login", (req, res, next) => {
-  res.render('components/login.ejs', {title: 'signup'})
+  res.render('components/login.ejs', {title: 'login'})
 })
 
 module.exports = router;
