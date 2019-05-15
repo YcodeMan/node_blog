513f5e23517833d5a5d1f2b164cb36fa25da4855
YcodeMan <1130578809@qq.com>
2019-05-15 20:27:43 +0800

    add sign_up,login router

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a  aaa');
 });
+
+// 配置注册页面模板
+router.get("/sign_up", (req, res, next) => {
+   res.render('components/signup.ejs', {title: 'signup'})
+})
+// 配置登录模板页面
+router.get("/login", (req, res, next) => {
+  res.render('components/login.ejs', {title: 'signup'})
+})
 
 module.exports = router;
