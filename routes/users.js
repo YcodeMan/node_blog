95eface936802f0bdae0da8806a04e412a58a8b2
YcodeMan <1130578809@qq.com>
2019-05-18 15:51:57 +0800

    pwd encrypt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose')
+const userModel = require('../models/users')
+const bcrypt = require('bcryptjs')
 
 // mongoose.connect("mongodb://localhost/shop")
 mongoose.connect("mongodb://localhost/node_blog", { useNewUrlParser: true }).then(
@@ -14,18 +16,40 @@ router.get('/', function(req, res, next) {
 
 // 配置注册页面模板
 router.get("/sign_up", (req, res, next) => {
-  res.render('components/signup.ejs', {title: 'signup'})
+  res.render('signup.ejs', {title: 'signup'})
 })
 
 // 实现注册功能
 router.post('/register', (req, res, next) => {
-  if (req.body) {
-    console.log(body)
-  }
+    var username = req.body.username,
+        password = req.body.password,
+        email = req.body.email
+    if (!(!!username && !!password && !!email)) {
+      res.json({
+        errorCode: -1,
+        msg: '未输入数据,注册失败',
+        data: {}
+      })
+    } else {
+      var salt = bcrypt.genSalt()
+          password = bcrypt.hash(password, salt)
+      userModel.find({
+        "username": username
+      }, (err, data) => {
+        if (data.length != 0) {
+          res.json({
+            errorCode: 333
+          })
+        } else {
+          
+        }
+      })
+    }    
+
 })
 // 配置登录模板页面
 router.get("/login", (req, res, next) => {
-  res.render('components/login.ejs', {title: 'login'})
+  res.render('login.ejs', {title: 'login'})
 })
 
 module.exports = router;
