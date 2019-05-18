ed4c38947ad00ba541d59b95ee1a9f4958c2c40d
YcodeMan <1130578809@qq.com>
2019-05-18 18:18:52 +0800

    achieve post way register

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,12 @@ const mongoose = require('mongoose')
 const userModel = require('../models/users')
 const bcrypt = require('bcryptjs')
 
-// mongoose.connect("mongodb://localhost/shop")
+// 连接数据库
 mongoose.connect("mongodb://localhost/node_blog", { useNewUrlParser: true }).then(
   () => { console.log("connect ok") },
   err => { console.log('connect err') }
 )
+mongoose.set('useCreateIndex', true)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('main/index.ejs', { title: 'Express' });
@@ -16,7 +17,7 @@ router.get('/', function(req, res, next) {
 
 // 配置注册页面模板
 router.get("/sign_up", (req, res, next) => {
-  res.render('signup.ejs', {title: 'signup'})
+  res.render('signup.ejs')
 })
 
 // 实现注册功能
@@ -24,32 +25,52 @@ router.post('/register', (req, res, next) => {
     var username = req.body.username,
         password = req.body.password,
         email = req.body.email
+        // 验证信息是否存在
     if (!(!!username && !!password && !!email)) {
       res.json({
         errorCode: -1,
-        msg: '未输入数据,注册失败',
+        msg: '未提交数据,注册失败',
         data: {}
       })
     } else {
-      var salt = bcrypt.genSalt()
-          password = bcrypt.hash(password, salt)
+      // 数据库中查找用户是否存在
       userModel.find({
-        "username": username
+        $or: [
+          {"username": username},
+          {"email": email}
+        ]
       }, (err, data) => {
         if (data.length != 0) {
           res.json({
-            errorCode: 333
+            errorCode: 501,
+            msg: '用户或者邮箱已存在',
+            data: {}
           })
         } else {
-          
+          // 给密码加密(同步)
+          var salt = bcrypt.genSaltSync()
+          password = bcrypt.hashSync(password, salt)
+
+          userModel.create({
+            username,
+            password,
+            email
+          }, (err, data) => {
+            if (err) throw err
+            res.json({
+              errorCode: 200,
+              msg: '用户注册成功',
+              data: {}
+            })
+          })
         }
       })
     }    
 
 })
 // 配置登录模板页面
-router.get("/login", (req, res, next) => {
-  res.render('login.ejs', {title: 'login'})
+router.get("/sign_in", (req, res, next) => {
+  res.render('signin.ejs',)
 })
 
 module.exports = router;
